8fa59d3762c7b51c924598e94143fe2fd63d60f6
Bangkit Haqi Aliaffuan <uwawuwuz@gmail.com>
2024-10-13 21:11:46 +0700

    Episode #42

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'idmenu',
+        'menu',
+        'harga',
+        'gambar',
+        'idkategori',
+        'harga',
+    ];
 }
